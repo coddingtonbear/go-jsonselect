6985731bcc1b93f2497e8d4c6fc7f4a7b5da8ce2
Adam Coddington <me@adamcoddington.net>
2013-11-17 16:28:05 -0800

    Adding tests.

--- jsonselect_test.go
+++ jsonselect_test.go
@@ -12,15 +12,14 @@ func getTestParser(testDocuments map[string]*simplejson.Json, testName string) (
     return CreateParser(jsonDocument)
 }
 
-func TestLevel1(t *testing.T) {
+func runTestsInDirectory(t *testing.T, baseDirectory string) {
     var testDocuments = make(map[string]*simplejson.Json)
     var testSelectors = make(map[string]string)
     var testOutput = make(map[string][]string)
-    var baseDirectory = "./conformance_tests/level_1/"
 
     files, err := ioutil.ReadDir(baseDirectory)
     if err != nil {
-        t.Errorf("Error encountered while loading conformance tests ", err)
+        t.Error("Error encountered while loading conformance tests ", err)
     }
 
     for _, fileInfo := range(files) {
@@ -28,29 +27,32 @@ func TestLevel1(t *testing.T) {
         if strings.HasSuffix(name, ".json") {
             json_document, err := ioutil.ReadFile(baseDirectory + name)
             if err != nil {
-                t.Errorf("Error encountered while reading ", name, ": ", err)
+                t.Error("Error encountered while reading ", name, ": ", err)
                 continue
             }
             parsed_document, err := simplejson.NewJson(json_document)
             if err != nil {
-                t.Errorf("Error encountered while deserializing ", name, ": ", err)
+                t.Error("Error encountered while deserializing ", name, ": ", err)
                 continue
             }
             testDocuments[name[0:len(name)-len(".json")]] = parsed_document
         } else if (strings.HasSuffix(name, ".output")) {
             output_document, err := ioutil.ReadFile(baseDirectory + name)
             if err != nil {
-                t.Errorf("Error encountered while reading ", name, ": ", err)
+                t.Error("Error encountered while reading ", name, ": ", err)
                 continue
             }
-            testOutput[name[0:len(name)-len(".output")]] = strings.Split(
-                string(output_document),
-                "\n",
-            )
+            var actualOutput []string
+            for _, str := range strings.Split(string(output_document), "\n") {
+                if len(str) > 0 {
+                    actualOutput = append(actualOutput, str)
+                }
+            }
+            testOutput[name[0:len(name)-len(".output")]] = actualOutput
         } else if (strings.HasSuffix(name, ".selector")) {
             selector_document, err := ioutil.ReadFile(baseDirectory + name)
             if err != nil {
-                t.Errorf("Error encountered while reading ", name, ": ", err)
+                t.Error("Error encountered while reading ", name, ": ", err)
                 continue
             }
             testSelectors[name[0:len(name)-len(".selector")]] = string(selector_document)
@@ -60,34 +62,46 @@ func TestLevel1(t *testing.T) {
     for testName := range testSelectors {
         parser, err := getTestParser(testDocuments, testName)
         if err != nil {
-            t.Errorf("Unable to find testing document for ", testName)
+            t.Error("Unable to find testing document for ", testName)
         }
         selectorString := testSelectors[testName]
         expectedOutput := testOutput[testName]
 
         results, err := parser.GetJsonElements(selectorString)
         if err != nil {
-            t.Errorf("Error encountered while finding matches ", err)
+            t.Error("Error encountered while finding matches ", err)
         }
         var stringResults []string
         for _, result := range results {
             encoded, err := result.Encode()
             if err != nil {
-                t.Errorf("Error encoding result '", result, "' in to JSON")
+                t.Error("Error encoding result '", result, "' in to JSON")
             }
             stringResults = append(stringResults, string(encoded))
         }
 
         if len(stringResults) != len(expectedOutput) {
-            t.Errorf("Number of results does not match: ", results, " != ", expectedOutput)
+            t.Error("Test ", testName, " failed due to number of results being mismatched: [Actual] ", stringResults, " (", len(stringResults), ") != [Expected] ", expectedOutput, " (", len(expectedOutput), ")")
         }
         for idx, result := range stringResults {
             expectedEncoded := expectedOutput[idx]
             if expectedEncoded != result {
-                t.Errorf(
-                    "Test ", testName, " failed on item #", idx, ": ", result, " != ", expectedEncoded,
+                t.Error(
+                    "Test ", testName, " failed on item #", idx, ": [Actual] ", result, " != [Expected] ", expectedEncoded,
                 )
             }
         }
     }
 }
+
+func TestLevel1(t *testing.T) {
+    runTestsInDirectory(t, "./conformance_tests/level_1/")
+}
+
+func TestLevel2(t *testing.T) {
+    runTestsInDirectory(t, "./conformance_tests/level_2/")
+}
+
+//func xTestLevel3(t *testing.T) {
+//    runTestsInDirectory(t, "./conformance_tests/level_3/")
+//}
